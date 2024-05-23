b76c7e8112ed23fe57636eaa172f96a6c7363ca2
Egemen <developer.egemen@gmail.com>
2024-05-23 16:36:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-tasks = []
+# MongoDB connection
+client = MongoClient('mongodb://34.132.34.71:27017/')
+db = client['tasks_db']
+tasks_collection = db['tasks']
 
 @app.route('/')
 def index():
-    return render_template('index.html', tasks=tasks)
+    tasks = list(tasks_collection.find({}, {'_id': 0, 'task': 1}))
+    return render_template('index.html', tasks=[task['task'] for task in tasks])
 
 @app.route('/add', methods=['POST'])
 def add():
     task = request.form.get('task')
     if task:
-        tasks.append(task)
+        tasks_collection.insert_one({'task': task})
     return redirect(url_for('index'))
 
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
+    tasks = list(tasks_collection.find({}, {'_id': 0, 'task': 1}))
     if 0 <= task_id < len(tasks):
-        tasks.pop(task_id)
+        task_to_delete = tasks[task_id]['task']
+        tasks_collection.delete_one({'task': task_to_delete})
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
